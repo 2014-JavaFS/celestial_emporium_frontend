08ff8bfccef8c9f68e217cfd7dc3f906174cd948
KayoJules <ChaoticPhoenix777@gmail.com>
2024-08-06 10:13:05 -0500

    Added bootstrap and react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Home from './pages/home/Home'
-import Nav from './components/nav/Nav'
-import Footer from './components/footer/Footer'
-import Login from './pages/login/Login'
-import Profile from './pages/profile/Profile'
-import Checkout from './pages/checkout/Checkout'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/home/Home";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Checkout from "./pages/checkout/Checkout";
 
 function App() {
-
-
   return (
-    <div className='App'>
-      <Nav/>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/profile" element={<Profile/>}></Route>
-          <Route path="/checkout" element={<Checkout/>}></Route>
-        </Routes>
-      </main>
-      
-      <Footer/>
+    <div className="App">
+      <Nav />
+      <Container>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+            <Route path="/checkout" element={<Checkout />}></Route>
+          </Routes>
+        </main>
+      </Container>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
