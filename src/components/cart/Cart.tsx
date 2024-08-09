9a2f86eec3a3ff9e48c3aedc38d76a6de0fb7d41
KayoJules <ChaoticPhoenix777@gmail.com>
2024-08-09 02:21:40 -0500

    Display cart items by user

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { ceServer } from "../../common/ce-server";
 import CartItem from "../cartitem/CartItem";
 
 interface cartInterface {
@@ -13,22 +12,24 @@ interface cartInterface {
 
 export default function AllCartItems() {
   const [cartItems, setCartItems] = useState([]);
+  const userId = 1; // Replace with the actual user ID from your authentication context or props
 
   useEffect(() => {
-    fetch("http://localhost:8080/cart-items", {
+    fetch(`http://localhost:8080/cart-items/${userId}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
     })
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
         setCartItems(data);
         console.log(data);
+      })
+      .catch((err) => {
+        console.error("Error fetching cart items:", err);
       });
-  }, []);
+  }, [userId]);
 
   return (
     <>
